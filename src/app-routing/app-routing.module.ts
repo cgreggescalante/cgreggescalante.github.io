7204c1435e439ae48a40cb3db2d885cfd3a14879
Conor <conor@johngregg.org>
2022-02-26 12:39:34 -0600

    Changed base url and internal links

--- src/app-routing/app-routing.module.ts
+++ src/app-routing/app-routing.module.ts
@@ -6,8 +6,7 @@ import {RouterModule, Routes} from "@angular/router";
 import {ProjectsComponent} from "../projects/projects.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full'},
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent},
   { path: 'projects', component: ProjectsComponent },
   { path: 'about', component: AboutComponent },
 ]
