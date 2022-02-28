30a47208528ba289918986ecda85a610cb95d023
Conor <conor@johngregg.org>
2022-02-28 11:44:46 -0600

    Added footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { AppRoutingModule } from "../app-routing/app-routing.module";
 import { ScullyLibModule } from '@scullyio/ng-lib';
+import {FooterComponent} from "../footer/footer.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
